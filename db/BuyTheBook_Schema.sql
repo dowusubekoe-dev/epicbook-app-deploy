4f3188df179f710feaafb4d76ed1242b8fcd9931
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2026-04-19 08:51:34 -0400

    drop table

--- db/BuyTheBook_Schema.sql
+++ db/BuyTheBook_Schema.sql
@@ -1,6 +1,11 @@
 /**
 On your local machine, replace 'epicbookdb' with 'epicbookdb_db'
 **/
+-- Add these to the very top of your BuyTheBook_Schema.sql file
+/*DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Cart;
+*/
 
 -- Use JAWS_DB Database
 USE epicbookdb;
@@ -13,7 +18,7 @@ USE epicbookdb;
 --   PRIMARY KEY (`id`));
 
 -- Create Table Author on DB
-CREATE TABLE `epicbookdb`.`Author` (
+CREATE TABLE IF NOT EXISTS `epicbookdb`.`Author` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `firstName` VARCHAR(45) NOT NULL,
   `lastName` VARCHAR(45) NOT NULL,
@@ -22,7 +27,7 @@ CREATE TABLE `epicbookdb`.`Author` (
   PRIMARY KEY (`id`));
 
 -- Create Table Book after Author because of FK constraint to Author Tbl
-CREATE TABLE `epicbookdb`.`Book` (
+CREATE TABLE IF NOT EXISTS`epicbookdb`.`Book` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `genre` VARCHAR(255) NOT NULL,
@@ -42,9 +47,8 @@ CREATE TABLE `epicbookdb`.`Book` (
     ON UPDATE NO ACTION);
 
 
-
 -- Create Table Cart
-CREATE TABLE `epicbookdb`.`Cart` (
+CREATE TABLE IF NOT EXISTS`epicbookdb`.`Cart` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `quantity` INT NOT NULL,
   `price` DECIMAL(13,2) NOT NULL,
